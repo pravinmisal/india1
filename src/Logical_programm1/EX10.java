0eedd1bc4f826e8292f663854e1f5386074b561a
pravinmisal <pravinmisal188@gmail.com>
2022-10-02 11:51:09 +0530

    test10

--- src/Logical_programm1/EX10.java
+++ src/Logical_programm1/EX10.java
@@ -3,5 +3,6 @@ package Logical_programm1;
 public class EX10 {
 public static void main(String[] args) {
 	System.out.println("ajay");
+	System.out.println("ajay got placed with 10lac package");
 }
 }
